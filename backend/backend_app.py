8470e9dc1944498e47c5879d884d1a2f04b99eb6
Alexander Kolle <alexanderkolle@MacBook-Air-von-Alexander.local>
2026-06-24 13:38:43 +0200

    implemented delete route

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -23,11 +23,11 @@ def add():
 
         title = new_post["title"]
         if title == "":
-            return jsonify("Bad request: Title is required."), 400
+            return jsonify("Bad request: 'Title' is required."), 400
 
         content = new_post['content']
         if content == "":
-            return jsonify("Bad request: Content is required."), 400
+            return jsonify("Bad request: 'Content' is required."), 400
 
         new_id = POSTS[-1]['id'] + 1 if POSTS else 1
 
@@ -40,6 +40,24 @@ def add():
         POSTS.append(new_post)
 
     return jsonify(POSTS), 201
+
+
+def find_book_by_id(post_id):
+    """ Find the book with the id `book_id`.
+    If there is no book with this id, return None. """
+    return next((post for post in POSTS if post['id'] == post_id), None)
+
+
+@app.route('/api/posts/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    post = find_book_by_id(id)
+
+    if post is None:
+        return jsonify("Post not Found"), 404
+
+    POSTS.remove(post)
+
+    return jsonify({"message": f'Post with id {id} has been deleted successfully.'}), 200
 
 
 if __name__ == '__main__':
